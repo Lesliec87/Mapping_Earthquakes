75662ec73b60c777045cd22e9b07d47af7fce85f
Your Name <you@example.com>
2022-05-29 10:36:44 -0500

    update

--- Mapping_Multiple_Points/static/js/cities.js
+++ Mapping_Multiple_Points/static/js/cities.js
@@ -45,8 +45,8 @@ cityData.forEach(function(city) {
     console.log(city)
     L.circleMarker(city.location, {
         radius: city.population/100000
-
     })
+    .setStyle({color: 'orange'})
     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
     .addTo(map);
 }); 
